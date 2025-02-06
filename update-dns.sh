2f9c64c8596828f5ecdbcb9891cf9d342895c1f4
jsllxx77 <55741323+jsllxx77@users.noreply.github.com>
2025-02-06 22:59:39 +0800

    Update update-dns.sh

--- update-dns.sh
+++ update-dns.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# 检查环境变量并设置默认值
-dns1=${DNS1:-"8.8.8.8"}
-dns2=${DNS2:-"8.8.4.4"}
-
-# 提示用户输入 DNS 服务器（如果未通过环境变量传递）
-if [ -z "$DNS1" ]; then
-    read -p "请输入第一个 DNS 服务器: " dns1
-fi
-
-if [ -z "$DNS2" ]; then
-    read -p "请输入第二个 DNS 服务器 (可选, 按回车跳过): " dns2
-fi
+# 提示用户输入 DNS 服务器
+read -p "请输入第一个 DNS 服务器: " dns1
+read -p "请输入第二个 DNS 服务器 (可选, 按回车跳过): " dns2
 
 # 备份原始的 resolv.conf 文件
 sudo cp /etc/resolv.conf /etc/resolv.conf.bak
